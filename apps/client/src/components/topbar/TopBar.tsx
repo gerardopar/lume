dadc414ecb731704ca6730dc83ff329bb6cf64c8
Gerardo Paredes <gerardparedes23@gmail.com>
2025-09-11 00:53:21 -0700

    add top bar

--- apps/client/src/components/topbar/TopBar.tsx
+++ apps/client/src/components/topbar/TopBar.tsx
@@ -1,7 +1,39 @@
 import React from "react";
+import BellIcon from "@components/svgs/BellIcon";
+import SearchIcon from "@components/svgs/SearchIcon";
 
 export const TopBar: React.FC = () => {
-  return <div className="w-full"></div>;
+  return (
+    <header className="w-full mb-6">
+      <div className="flex items-center justify-between gap-4">
+        <div className="flex items-center flex-1 max-w-[60%] relative">
+          <input
+            type="text"
+            placeholder="Search"
+            className={`
+              w-full bg-lume-secondary-dark rounded-full p-2 py-[10px] placeholder:text-lume-primary-light 
+              focus:outline-none placeholder:font-poppins placeholder:font-[200] placeholder:text-[16px]
+               pl-4
+              `}
+          />
+
+          <SearchIcon className="h-5 w-5 text-lume-primary-light absolute right-4 top-1/2 -translate-y-1/2" />
+        </div>
+
+        <div className="flex items-center gap-2">
+          <div className="flex items-center p-[10px] rounded-full bg-lume-secondary-dark">
+            <BellIcon className="w-5 h-5 text-lume-primary-light" />
+          </div>
+          <div className="rounded-full overflow-hidden bg-lume-secondary-dark h-[40px] w-[40px] flex justify-center items-center">
+            <img
+              className="w-full h-full object-cover"
+              src="https://img.daisyui.com/images/profile/demo/yellingcat@192.webp"
+            />
+          </div>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default TopBar;
--- apps/client/src/pages/Home/Home.tsx
+++ apps/client/src/pages/Home/Home.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 
 import Hero from "@components/hero/Hero";
+import TopBar from "@components/topbar/TopBar";
 import Sidenav from "@components/sidenav/Sidenav";
 
 export const Home: React.FC = () => {
@@ -11,6 +12,7 @@ export const Home: React.FC = () => {
 
       {/* right handle panel */}
       <div className="h-full w-full flex flex-col px-6">
+        <TopBar />
         <Hero />
       </div>
     </div>
