af0f6b35277d7eb739d6dff3839cca0ac8957ae2
Gerardo Paredes <gerardparedes23@gmail.com>
2025-09-11 00:23:07 -0700

    setup basic sidenav

--- apps/client/src/components/sidenav/Sidenav.tsx
+++ apps/client/src/components/sidenav/Sidenav.tsx
@@ -1,23 +1,34 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 import { sidenavLinks } from "./sidenav-helpers";
 
 export const Sidenav: React.FC = () => {
   return (
-    <div className="max-w-[260px] w-full bg-lume-secondary-dark rounded-2xl flex flex-col p-6">
-      {sidenavLinks.map((link, index) => (
-        <a
-          key={link.id}
-          href={link.path}
-          className="text-lume-primary-light font-poppins font-[200] text-lg py-2"
-        >
-          {link.label}
+    <div className="max-w-[240px] w-full bg-lume-secondary-dark rounded-2xl flex flex-col p-6">
+      {sidenavLinks.map((link, index) => {
+        const { id, Icon, label, path } = link;
 
-          {index === 2 && (
-            <div className="h-[1px] bg-lume-primary-light/5 w-full mt-4" />
-          )}
-        </a>
-      ))}
+        return (
+          <>
+            <NavLink
+              key={id}
+              to={path}
+              className={({ isActive }) =>
+                `flex items-center  font-poppins font-[200] text-lg py-2 ${
+                  isActive ? "text-lume-green" : "text-lume-primary-light"
+                }`
+              }
+            >
+              <Icon className="w-5 h-5 mr-2" />
+              {label}
+            </NavLink>
+            {index === 2 && (
+              <div className="h-[1px] bg-lume-primary-light/10 my-2" />
+            )}
+          </>
+        );
+      })}
     </div>
   );
 };
--- apps/client/src/components/sidenav/sidenav-helpers.ts
+++ apps/client/src/components/sidenav/sidenav-helpers.ts
@@ -1,28 +1,47 @@
-export const sidenavLinks = [
+import { HomeIcon } from "../svgs/HomeIcon";
+import { UserIcon } from "../svgs/UserIcon";
+import { HeartIcon } from "../svgs/HeartIcon";
+import { CompassIcon } from "../svgs/CompassIcon";
+import { SettingsIcon } from "../svgs/SettingsIcon";
+import React from "react";
+
+export interface SidenavLink {
+  id: number;
+  label: string;
+  path: string;
+  Icon: React.FC<{ className?: string }>;
+}
+
+export const sidenavLinks: SidenavLink[] = [
   {
     id: 1,
     label: "Home",
     path: "/",
+    Icon: HomeIcon,
   },
   {
     id: 2,
     label: "Explore",
     path: "/explore",
+    Icon: CompassIcon,
   },
   {
     id: 3,
     label: "Favorites",
     path: "/favorites",
+    Icon: HeartIcon,
   },
 
   {
     id: 4,
     label: "Profile",
     path: "/profile",
+    Icon: UserIcon,
   },
   {
     id: 5,
     label: "Settings",
     path: "/settings",
+    Icon: SettingsIcon,
   },
 ];
