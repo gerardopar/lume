43d2c6c3b426532621df4869951e893dfbfcef0d
Gerardo Paredes <gerardparedes23@gmail.com>
2025-09-10 20:44:55 -0700

    use tailwind v4 config

--- apps/client/src/pages/Home/Home.tsx
+++ apps/client/src/pages/Home/Home.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import { Hero } from "@components/hero/Hero";
-import { Header } from "@components/header/Header";
 
 export const Home: React.FC = () => {
   return (
-    <div className="h-screen w-full bg-black">
-      <Header />
+    <div className="h-screen w-full bg-lume-primary-dark p-6">
+      {/* left handle panel */}
+      <div className="max-[300px] w-full"></div>
+
+      {/* right handle panel */}
       <div className="h-full w-full flex flex-col">
         <Hero />
       </div>
