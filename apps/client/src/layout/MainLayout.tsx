583fcd2c6f0b27222e420a70cd99bf678bf6fd2e
Gerardo Paredes <gerardparedes23@gmail.com>
2025-09-11 21:03:42 -0700

    add app mainlayout

---
+++ apps/client/src/layout/MainLayout.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+import Sidenav from "@components/sidenav/Sidenav";
+
+export const MainLayout: React.FC<{ children: React.ReactNode }> = ({
+  children,
+}) => {
+  return (
+    <div className="h-screen flex w-full bg-lume-primary-dark p-6">
+      {/* side nav panel */}
+      <Sidenav />
+
+      {/* main content panel */}
+      <div className="h-full w-full flex flex-col px-6 overflow-y-scroll">
+        {children}
+      </div>
+    </div>
+  );
+};
+
+export default MainLayout;
