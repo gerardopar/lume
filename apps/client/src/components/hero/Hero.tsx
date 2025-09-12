b6f8a3f5d5c973078eebcd5f8d72dd4128022669
Gerardo Paredes <gerardparedes23@gmail.com>
2025-09-11 18:27:03 -0700

    wire up hero

--- apps/client/src/components/hero/Hero.tsx
+++ apps/client/src/components/hero/Hero.tsx
@@ -2,6 +2,8 @@ import React from "react";
 
 import { buildImageUrl, config } from "../../helpers/tmdb-image.helpers";
 
+import { getRandomMovie, getGenres } from "../../helpers/movie.helpers";
+
 import { trpc } from "@utils/trpc";
 
 export const Hero: React.FC = () => {
@@ -9,22 +11,51 @@ export const Hero: React.FC = () => {
     page: 1,
   });
 
-  const movie = data?.results[0];
+  const movie = getRandomMovie(data?.results ?? []);
+  const { title, overview, backdrop_path } = movie ?? {};
 
-  const poster = buildImageUrl(
-    config,
-    "backdrop",
-    movie?.backdrop_path!,
-    "original"
-  );
+  const genres = getGenres(movie!, 3);
+  const poster = buildImageUrl(config, "backdrop", backdrop_path!, "original");
 
   return (
-    <div className="h-[40%] w-full rounded-2xl overflow-hidden">
-      <img
-        src={poster!}
-        alt="backdrop"
-        className="w-full h-full object-cover object-center"
-      />
+    <div
+      className="h-[50%] w-full flex flex-col rounded-2xl overflow-hidden p-6"
+      style={{
+        backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(${poster})`,
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    >
+      <div className="w-full flex items-center justify-end">
+        <div className="shadow-lg text-base inline px-6 py-2 rounded-full backdrop-blur-lg bg-white/10">
+          Now Playing
+        </div>
+      </div>
+
+      <div className="w-full h-full flex flex-col justify-end gap-2">
+        <div className="flex items-center gap-2">
+          {genres.map((genre) => (
+            <span
+              key={genre}
+              className="shadow-lg text-xs inline px-4 py-1 rounded-full backdrop-blur-lg bg-white/10"
+            >
+              {genre}
+            </span>
+          ))}
+        </div>
+        <h1 className="text-4xl font-bold font-inter text-white">{title}</h1>
+        <p className="text-base font-poppins text-white font-[200] max-w-[40%] line-clamp-3">
+          {overview}
+        </p>
+        <div className="flex items-center gap-2">
+          <button className="shadow-lg btn font-[400] bg-lume-primary-dark text-lume-primary-light rounded-full hover:bg-lume-primary-dark/50 hover:border-lume-primary-dark/50">
+            Watch Trailer
+          </button>
+          <button className="shadow-lg btn btn-outline font-[400] rounded-full text-lume-primary-light hover:bg-lume-primary-light hover:text-lume-primary-dark hover:border-lume-primary-light">
+            Add to Watchlist
+          </button>
+        </div>
+      </div>
     </div>
   );
 };
