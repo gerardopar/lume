e3a43274879bb272b2880d6c7e5de0b152220121
Gerardo Paredes <gerardparedes23@gmail.com>
2025-09-13 00:14:43 -0700

    add filter active state

--- apps/client/src/components/SuggestionsInput/SuggestionsFilterDropdown.tsx
+++ apps/client/src/components/SuggestionsInput/SuggestionsFilterDropdown.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 
+import CheckmarkIcon from "@components/svgs/CheckmarkIcon";
 import ChevronDownIcon from "@components/svgs/ChevronDownIcon";
 
 import { filterOptions, FilterOptionEnum } from "./suggestions-input.helpers";
@@ -9,11 +10,11 @@ export const SuggestionsFilterDropdown: React.FC<{
   setActiveFilter: (value: FilterOptionEnum) => void;
 }> = ({ activeFilter, setActiveFilter }) => {
   return (
-    <div className="dropdown dropdown-bottom">
+    <div className="dropdown dropdown-bottom min-w-[118px] w-[118px]">
       <div
         tabIndex={0}
         role="button"
-        className="font-poppins font-[200] capitalize px-4 py-[10px] rounded-full flex items-center justify-center bg-lume-secondary-dark text-white mr-2"
+        className="font-poppins font-[200] capitalize px-4 py-[10px] rounded-full flex items-center justify-between bg-lume-secondary-dark text-white mr-2"
       >
         {activeFilter}
         <ChevronDownIcon className="ml-2" />
@@ -23,13 +24,21 @@ export const SuggestionsFilterDropdown: React.FC<{
         className="dropdown-content menu bg-lume-secondary-dark rounded-box z-1 w-52 shadow-sm mt-2 p-0"
       >
         {filterOptions.map((option) => (
-          <li key={option.id}>
+          <li
+            key={option.id}
+            className={
+              activeFilter === option.value ? "bg-lume-primary-dark/50" : ""
+            }
+          >
             <button
               type="button"
-              className="hover:bg-lume-primary-dark/50 text-lume-primary-light p-2 pl-4 font-poppins font-[200] text-base"
+              className="hover:bg-lume-primary-dark/50 text-lume-primary-light p-2 px-4 font-poppins font-[200] text-base flex items-center justify-between"
               onClick={() => setActiveFilter(option.value)}
             >
               {option.label}
+              {activeFilter === option.value && (
+                <CheckmarkIcon strokeWidth="1" />
+              )}
             </button>
           </li>
         ))}
---
+++ apps/client/src/components/svgs/CheckmarkIcon.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+export const CheckmarkIcon: React.FC<{
+  className?: string;
+  strokeWidth?: string;
+}> = ({ className, strokeWidth }) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M20 6L9 17L4 12"
+        stroke="white"
+        strokeWidth={strokeWidth ?? "2"}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default CheckmarkIcon;
