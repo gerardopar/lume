297aada84aae646b8eaf161e7e08ecb3a80637fe
Gerardo Paredes <gerardparedes23@gmail.com>
2025-09-14 13:32:23 -0700

    add user context, add protectedProcedure

--- apps/server/src/trpc.ts
+++ apps/server/src/trpc.ts
@@ -1,6 +1,20 @@
+// apps/server/src/trpc/trpc.ts
 import { initTRPC } from "@trpc/server";
+import type { Context } from "./context";
 
-const t = initTRPC.create();
+const t = initTRPC.context<Context>().create();
+
+const isAuthed = t.middleware(({ ctx, next }) => {
+  if (!ctx.user) {
+    throw new Error("UNAUTHORIZED");
+  }
+  return next({
+    ctx: {
+      user: ctx.user,
+    },
+  });
+});
 
 export const router = t.router;
 export const publicProcedure = t.procedure;
+export const protectedProcedure = t.procedure.use(isAuthed);
