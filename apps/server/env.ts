3b4dc14babd8c9a228a7c4292122ef1ca11a0d70
Gerardo Paredes <gerardparedes23@gmail.com>
2025-09-14 13:56:29 -0700

    update env schema, add firebase, add s3

--- apps/server/env.ts
+++ apps/server/env.ts
@@ -14,6 +14,11 @@ const envSchema = z.object({
   MONGODB_HOST: z.string(),
   MONGODB_DB: z.string(),
   MONGODB_APPNAME: z.string(),
+  FIREBASE_SERVICE_ACCOUNT: z.string(),
+  AWS_ACCESS_KEY_ID: z.string(),
+  AWS_SECRET_ACCESS_KEY: z.string(),
+  AWS_S3_BUCKET_NAME: z.string(),
+  AWS_S3_BUCKET_REGION: z.string(),
 });
 
 export const env = envSchema.parse({
