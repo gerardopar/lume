c85a8c492aea7afec3b59584b1fd0871b6a727cb
Gerardo Paredes <gerardparedes23@gmail.com>
2025-09-15 15:31:26 -0700

    add toast component

---
+++ apps/client/src/components/toast/Toast.tsx
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+
+import ReactDOM from "react-dom";
+import { AnimatePresence, motion } from "motion/react";
+
+import { toastStore, ToastTypesEnum } from "../../stores/toasts";
+
+export const Toast: React.FC = () => {
+  const content = toastStore.useTracked("content");
+  const isOpen = toastStore.useTracked("isOpen");
+  const { type, duration } = toastStore.useTracked("options");
+
+  const { close } = toastStore.actions;
+
+  useEffect(() => {
+    if (duration) {
+      setTimeout(() => close(), duration);
+    }
+  }, [duration]);
+
+  // Map modal type to className
+  const toastTypeMap: Record<ToastTypesEnum, string> = {
+    [ToastTypesEnum.Start]: "toast-start",
+    [ToastTypesEnum.Center]: "toast-center",
+    [ToastTypesEnum.End]: "toast-end",
+    [ToastTypesEnum.Top]: "toast-top",
+    [ToastTypesEnum.Middle]: "toast-middle",
+    [ToastTypesEnum.Bottom]: "toast-bottom",
+  };
+
+  const toastType = toastTypeMap[type || ToastTypesEnum.End];
+
+  if (!content || !isOpen) return null;
+
+  return ReactDOM.createPortal(
+    <AnimatePresence>
+      {isOpen && (
+        <motion.div className={`${toastType} toast`}>{content}</motion.div>
+      )}
+    </AnimatePresence>,
+    document.body
+  );
+};
+
+export default Toast;
