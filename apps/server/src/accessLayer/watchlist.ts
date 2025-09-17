46d98dab922039361ac41fb692dec5031ee5f008
Gerardo Paredes <gerardparedes23@gmail.com>
2025-09-16 21:35:56 -0700

    setup watchlist model, router

---
+++ apps/server/src/accessLayer/watchlist.ts
@@ -0,0 +1,126 @@
+import { Types } from "mongoose";
+import { TRPCError } from "@trpc/server";
+import { WatchlistItem } from "../models/watchlist";
+
+type CreateWatchlistInput = {
+  userId: string;
+  tmdbId: number;
+  mediaType: "movie" | "tv";
+  title: string;
+  posterPath?: string;
+  releaseDate?: string;
+  overview?: string;
+  voteAverage?: number;
+};
+
+export const createWatchlistItem = async (input: CreateWatchlistInput) => {
+  try {
+    const watchlistItem = await WatchlistItem.findOneAndUpdate(
+      {
+        userId: new Types.ObjectId(input.userId),
+        tmdbId: input.tmdbId,
+        mediaType: input.mediaType,
+      },
+      { $setOnInsert: input },
+      { new: true, upsert: true }
+    ).exec();
+
+    return watchlistItem;
+  } catch (error) {
+    console.error(error);
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Unable to create watchlist item",
+    });
+  }
+};
+
+export const getWatchlistItemsByUser = async (userId: string) => {
+  try {
+    const watchlistItems = await WatchlistItem.find({
+      userId: new Types.ObjectId(userId),
+    })
+      .sort({ createdAt: -1 })
+      .lean()
+      .exec();
+
+    return watchlistItems;
+  } catch (error) {
+    console.error(error);
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Unable to get watchlist items",
+    });
+  }
+};
+
+export const getWatchlistItemById = async (id: string) => {
+  try {
+    const watchlistItem = await WatchlistItem.findById(id).exec();
+
+    if (!watchlistItem) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Watchlist item not found",
+      });
+    }
+
+    return watchlistItem;
+  } catch (error) {
+    console.error(error);
+    if (error instanceof TRPCError) throw error;
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Unable to get watchlist item",
+    });
+  }
+};
+
+export const updateWatchlistItem = async (
+  id: string,
+  input: Partial<CreateWatchlistInput>
+) => {
+  try {
+    const watchlistItem = await WatchlistItem.findByIdAndUpdate(id, input, {
+      new: true,
+    }).exec();
+
+    if (!watchlistItem) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Watchlist item not found",
+      });
+    }
+
+    return watchlistItem;
+  } catch (error) {
+    console.error(error);
+    if (error instanceof TRPCError) throw error;
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Unable to update watchlist item",
+    });
+  }
+};
+
+export const deleteWatchlistItem = async (id: string) => {
+  try {
+    const watchlistItem = await WatchlistItem.findByIdAndDelete(id).exec();
+
+    if (!watchlistItem) {
+      throw new TRPCError({
+        code: "NOT_FOUND",
+        message: "Watchlist item not found",
+      });
+    }
+
+    return watchlistItem;
+  } catch (error) {
+    console.error(error);
+    if (error instanceof TRPCError) throw error;
+    throw new TRPCError({
+      code: "INTERNAL_SERVER_ERROR",
+      message: "Unable to delete watchlist item",
+    });
+  }
+};
---
+++ apps/server/src/routers/watchlist.tsx
@@ -0,0 +1,105 @@
+import { z } from "zod";
+import { TRPCError } from "@trpc/server";
+import { protectedProcedure, router } from "../trpc";
+
+import { getUserByFirebaseUid } from "../accessLayer/user";
+import {
+  createWatchlistItem,
+  updateWatchlistItem,
+  deleteWatchlistItem,
+  getWatchlistItemsByUser,
+  getWatchlistItemById,
+} from "../accessLayer/watchlist";
+import {
+  CreateWatchlistItemSchema,
+  UpdateWatchlistItemSchema,
+} from "../validators/watchlist";
+
+export const moviesRouter = router({
+  createWatchlistItem: protectedProcedure
+    .input(CreateWatchlistItemSchema)
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const user = await getUserByFirebaseUid(ctx.user?.uid!);
+        const watchlistItem = await createWatchlistItem({
+          ...input,
+          userId: user?._id?.toString()!,
+        });
+        return watchlistItem;
+      } catch (error) {
+        console.error(error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Unable to create watchlist item",
+        });
+      }
+    }),
+  updateWatchlistItem: protectedProcedure
+    .input(
+      z.object({
+        id: z.string(),
+        input: UpdateWatchlistItemSchema,
+      })
+    )
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const user = await getUserByFirebaseUid(ctx.user?.uid!);
+        // TODO check if user is the owner of the watchlist item
+        const watchlistItem = await updateWatchlistItem(input.id, input.input);
+        return watchlistItem;
+      } catch (error) {
+        console.error(error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Unable to update watchlist item",
+        });
+      }
+    }),
+  deleteWatchlistItem: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .mutation(async ({ ctx, input }) => {
+      try {
+        const user = await getUserByFirebaseUid(ctx.user?.uid!);
+        // TODO check if user is the owner of the watchlist item
+        const watchlistItem = await deleteWatchlistItem(input.id);
+        return watchlistItem;
+      } catch (error) {
+        console.error(error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Unable to delete watchlist item",
+        });
+      }
+    }),
+  getWatchlistItemsByUser: protectedProcedure.query(async ({ ctx }) => {
+    try {
+      const user = await getUserByFirebaseUid(ctx.user?.uid!);
+      const watchlistItems = await getWatchlistItemsByUser(
+        user?._id?.toString()!
+      );
+      return watchlistItems;
+    } catch (error) {
+      console.error(error);
+      throw new TRPCError({
+        code: "INTERNAL_SERVER_ERROR",
+        message: "Unable to get watchlist items",
+      });
+    }
+  }),
+  getWatchlistItemById: protectedProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ ctx, input }) => {
+      try {
+        const user = await getUserByFirebaseUid(ctx.user?.uid!);
+        // TODO check if user is the owner of the watchlist item
+        const watchlistItem = await getWatchlistItemById(input.id);
+        return watchlistItem;
+      } catch (error) {
+        console.error(error);
+        throw new TRPCError({
+          code: "INTERNAL_SERVER_ERROR",
+          message: "Unable to get watchlist item",
+        });
+      }
+    }),
+});
