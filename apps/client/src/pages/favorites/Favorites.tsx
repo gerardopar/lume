00c8ff2be0d187830b98b91ebba7dd5a520f4142
Gerardo Paredes <gerardparedes23@gmail.com>
2025-09-17 19:31:54 -0700

    add favorites & movies routes

---
+++ apps/client/src/pages/favorites/Favorites.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import MainLayout from "../../layout/MainLayout";
+
+export const Favorites: React.FC = () => {
+  return (
+    <MainLayout>
+      <div className="w-full">
+        <h1>Favorites</h1>
+      </div>
+    </MainLayout>
+  );
+};
+
+export default Favorites;
--- apps/client/src/router/AppRouter.tsx
+++ apps/client/src/router/AppRouter.tsx
@@ -3,15 +3,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Home } from "../pages/Home/Home";
 import { PrivateRoute } from "./PrivateRoute";
 import { WatchList } from "../pages/watchlist/WatchList";
+import { Favorites } from "../pages/favorites/Favorites";
 
 export const AppRouter = () => {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/movies" element={<Home />} />
         <Route
           path="/watchlist"
           element={<PrivateRoute children={<WatchList />} />}
+        />
+        <Route
+          path="/favorites"
+          element={<PrivateRoute children={<Favorites />} />}
         />
       </Routes>
     </BrowserRouter>
