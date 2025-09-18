60d9b4327d6c153ea09e73d05bc905077f27397c
Gerardo Paredes <gerardparedes23@gmail.com>
2025-09-17 19:30:53 -0700

    add watchlist placeholder

--- apps/client/src/pages/watchlist/WatchList.tsx
+++ apps/client/src/pages/watchlist/WatchList.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { trpc } from "@utils/trpc";
 
 import MainLayout from "../../layout/MainLayout";
+import WatchListPlaceholder from "../../components/empty-states/WatchListPlaceholder";
 
-export const WatchList = () => {
+export const WatchList: React.FC = () => {
+  const { data: watchlist, isLoading: watchlistLoading } =
+    trpc.watchlist.getWatchlistItemsByUser.useQuery();
+
   return (
     <MainLayout>
-      <div className="w-full">
-        <h1>WatchList</h1>
+      <div className="w-full mt-6">
+        <h1 className="font-inter font-bold text-4xl text-lume-primary-light">
+          My Watchlist
+        </h1>
+        <div className="mt-4 divider" />
+
+        {!watchlistLoading && watchlist?.length === 0 && (
+          <WatchListPlaceholder />
+        )}
       </div>
     </MainLayout>
   );
