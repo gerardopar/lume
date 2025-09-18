7fd0488826f4489e6521b19ccf92e61407298604
Gerardo Paredes <gerardparedes23@gmail.com>
2025-09-17 20:05:05 -0700

    add fagvorites empty placeholder

---
+++ apps/client/src/components/empty-states/FavoritesPlaceholder.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+
+import Lottie from "lottie-react";
+
+import emptyFavoritesAnimation from "../../components/lotties/heart.json";
+
+export const FavoritesPlaceholder: React.FC = () => {
+  const navigate = useNavigate();
+
+  return (
+    <div className="flex items-center justify-start mt-[100px] flex-1 flex-col">
+      <Lottie
+        animationData={emptyFavoritesAnimation}
+        loop
+        autoplay
+        className="w-64 h-64"
+      />
+      <h2 className="text-xl font-inter font-semibold mb-2 text-white">
+        Your favorites list is empty
+      </h2>
+      <p className="mb-6 text-gray-400 font-poppins font-[200] text-base">
+        Save movies and TV shows you love.
+      </p>
+      <div className="flex items-center gap-2">
+        <button
+          type="button"
+          onClick={() => navigate("/")}
+          className="bg-lume-green/90 text-white px-6 py-2 rounded-[10px] cursor-pointer hover:bg-lume-green/60 transition-colors duration-200 ease-in-out mr-2"
+        >
+          Browse Movies
+        </button>
+        <button
+          type="button"
+          onClick={() => navigate("/tv-shows")}
+          className="bg-lume-green/90 text-white px-6 py-2 rounded-[10px] cursor-pointer hover:bg-lume-green/60 transition-colors duration-200 ease-in-out"
+        >
+          Browse TV Shows
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default FavoritesPlaceholder;
--- apps/client/src/pages/favorites/Favorites.tsx
+++ apps/client/src/pages/favorites/Favorites.tsx
@@ -1,12 +1,24 @@
 import React from "react";
+import { trpc } from "@utils/trpc";
 
 import MainLayout from "../../layout/MainLayout";
+import FavoritesPlaceholder from "../../components/empty-states/FavoritesPlaceholder";
 
 export const Favorites: React.FC = () => {
+  const { data: favorites, isLoading: favoritesLoading } =
+    trpc.favorites.getFavoriteItemsByUser.useQuery();
+
   return (
     <MainLayout>
-      <div className="w-full">
-        <h1>Favorites</h1>
+      <div className="w-full mt-6">
+        <h1 className="font-inter font-bold text-4xl text-lume-primary-light">
+          My Favorites
+        </h1>
+        <div className="mt-4 divider" />
+
+        {!favoritesLoading && favorites?.length === 0 && (
+          <FavoritesPlaceholder />
+        )}
       </div>
     </MainLayout>
   );
