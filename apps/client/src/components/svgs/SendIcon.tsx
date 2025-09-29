636d03f9c3c3ed4aa4e1167bc1647e41528e3911
Gerardo Paredes <gerardparedes23@gmail.com>
2025-09-28 21:37:23 -0700

    add send icon

---
+++ apps/client/src/components/svgs/SendIcon.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+
+export const SendIcon: React.FC<{ className?: string }> = ({ className }) => {
+  return (
+    <svg
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+      className={className}
+    >
+      <path
+        d="M22 2L11 13"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+      <path
+        d="M22 2L15 22L11 13L2 9L22 2Z"
+        stroke="currentColor"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+};
+
+export default SendIcon;
