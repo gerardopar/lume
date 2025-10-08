add5fb33fe7ef4503c29467498580fcc8d557788
Gerardo Paredes <gerardparedes23@gmail.com>
2025-10-08 16:41:35 -0700

    add detailed recommendations view

--- apps/client/src/components/recommendations-chatbot/ChatBotQuestion.tsx
+++ apps/client/src/components/recommendations-chatbot/ChatBotQuestion.tsx
@@ -1,15 +1,20 @@
 import React from "react";
 import { motion } from "motion/react";
 
+import WatchlistButton from "@components/shared/WatchlistButton";
+
 import { chatbotStore } from "../../stores/chatbot";
 
+import { buildImageUrl, config } from "../../helpers/tmdb-image.helpers";
 import { type QA, QAEnum, qaIndex } from "./chatbot.helpers";
+import type { MediaItemSnapshot, TmdbMovie, TmdbTvShow } from "@my/api";
 
 export const ChatBotQuestion: React.FC<{
   qaItem: QA;
   index: number;
   isPending: boolean;
 }> = ({ qaItem, index, isPending }) => {
+  const qa = chatbotStore.useTracked("qa");
   const setQA = chatbotStore.actions.setQA;
   const setLastAnsweredIndex = chatbotStore.actions.setLastAnsweredIndex;
 
@@ -98,8 +103,75 @@ export const ChatBotQuestion: React.FC<{
       )}
 
       {qaItem.type === QAEnum.recommendations && qaItem.predefinedAnswers && (
-        <div>
-          <p>Recommendations</p>
+        <div className="w-full flex flex-nowrap overflow-x-scroll mt-4 rounded-2xl">
+          {Array.isArray(qaItem.predefinedAnswers) &&
+            qaItem.predefinedAnswers.map(
+              (
+                answer: Partial<TmdbMovie> | Partial<TmdbTvShow>,
+                idx: number
+              ) => {
+                const title =
+                  answer?.original_title || answer?.original_name || "";
+
+                const poster =
+                  answer?.poster_path || answer?.backdrop_path || "";
+
+                const posterUrl = buildImageUrl(
+                  config,
+                  "poster",
+                  poster,
+                  "original"
+                );
+
+                const mediaType =
+                  qa[qaIndex.moodFor].answer === "movie" ? "movie" : "tv";
+
+                const snapShot: MediaItemSnapshot =
+                  mediaType === "movie"
+                    ? {
+                        tmdbId: answer?.id,
+                        mediaType: "movie",
+                        title,
+                        posterPath: answer?.poster_path || "",
+                        releaseDate: answer?.release_date,
+                        overview: answer?.overview,
+                        voteAverage: answer?.vote_average,
+                        genreIds: answer?.genre_ids,
+                      }
+                    : {
+                        tmdbId: answer?.id,
+                        mediaType: "tv",
+                        title,
+                        posterPath: answer?.poster_path || "",
+                        releaseDate: answer?.first_air_date,
+                        overview: answer?.overview,
+                        voteAverage: answer?.vote_average,
+                        genreIds: answer?.genre_ids,
+                      };
+
+                return (
+                  <motion.div
+                    key={idx}
+                    className={`min-w-full h-[240px] snap-center bg-cover bg-top relative flex flex-col items-start justify-end p-4 rounded-2xl group gap-2 mr-2`}
+                    style={{ backgroundImage: `url(${posterUrl})` }}
+                  >
+                    <div
+                      className="
+                      rounded-2xl
+                          absolute inset-0 bg-gradient-to-b from-black/25 to-black/75
+                        "
+                    />
+                    <h1 className="w-full text-xl font-bold font-inter text-white z-10">
+                      {title}
+                    </h1>
+                    <p className="w-full text-sm font-poppins text-white font-[200] line-clamp-3 z-10">
+                      {answer?.overview}
+                    </p>
+                    <WatchlistButton tmdbId={answer.id!} snapshot={snapShot} />
+                  </motion.div>
+                );
+              }
+            )}
         </div>
       )}
     </motion.div>
